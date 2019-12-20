1a91c3823d717c89a0e6ce263bae0cbaf14f0b0d
Anthony Valente <valenta4@uw.edu>
2019-12-20 15:34:39 -0800

    Update test script

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
 if [ "$1" == "+" ]; then
-    snakemake --use-conda --shadow-prefix /tmp --directory test
+    snakemake --use-conda \
+              --latency-wait 60 \
+              --jobs 16 \
+              --cluster-config cluster.json \
+              --cluster "qsub -P {cluster.project} -p {cluster.priority} -pe serial {cluster.ncores} -l {cluster.resources} -o {cluster.eNo} -j y" \
+              --directory test
 else
-    snakemake -n --use-conda --shadow-prefix /tmp --directory test
+    snakemake -n --directory test
 fi
