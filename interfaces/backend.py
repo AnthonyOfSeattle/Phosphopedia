c2e88cff1fb321fc20c612294eed4deed98c83e5
Anthony Valente <valenta4@uw.edu>
2021-04-29 11:08:08 -0700

    Add database backend functions with exponential backoff

--- interfaces/backend.py
+++ interfaces/backend.py
@@ -1,56 +1,58 @@
+import time
 from sqlalchemy import create_engine
 from sqlalchemy.pool import NullPool
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.exc import OperationalError
 from .schema import *
 
-SAFE_QUERY_ITERS = 1000
-
-def safe_get_all(query):
-    for i in map(time.sleep, np.random.uniform(0, 5, size=SAFE_QUERY_ITERS)):
-        try:
-            result = query.all()
-            break
-        except OperationalError:
-            continue
-
-    return result
-
-def safe_get_one(query):
-    for i in map(time.sleep, np.random.uniform(0, 5, size=SAFE_QUERY_ITERS)):
-        try:
-            result = query.one()
-            break
-        except OperationalError:
-            continue
-
-    return result
-
-def safe_update(session, obj):
-    for i in map(time.sleep, np.random.uniform(0, 5, size=SAFE_QUERY_ITERS)):
-        try:
-            session.add(obj)
-            session.commit()
-            break
-        except OperationalError:
-            session.rollback()
 
 class DatabaseBackend:
-    def __init__(self, database_path):
+    def __init__(self, database_path, backoffs=100):
         self.database_path = database_path
+        self.backoffs = 100
 
-    def _get_engine(self):
+    def _backoff(self):
+        yield True
+        for attempt in range(self.backoffs):
+            time.sleep(0.1 * 2 ** attempt)
+            yield True
+
+        yield False
+
+    def create_engine(self):
         return create_engine(self.database_path, poolclass=NullPool)
 
-    def _get_session(self):
-        Session = scoped_session(sessionmaker(bind=self._get_engine()))
-        return Session
+    def create_session(self):
+        return scoped_session(sessionmaker(bind=self.create_engine()))
 
     def initialize_database(self):
-        with self._get_engine().connect() as connection:
+        with self.create_engine().connect() as connection:
             try:
                 PhosphopediaBase.metadata.create_all(
                     connection
                 )
             except OperationalError:
                 print("Database locked, ignoring initialization check.")
+
+    def safe_add(self, obj):
+        session = self.create_session()
+        while self._backoff():
+            try:
+                session.add(obj)
+                session.commit()
+                session.remove()
+                return True
+            except OperationalError:
+                session.rollback()
+
+        session.remove()
+        return False
+
+    def safe_run(self, func):
+        while self._backoff():
+            try:
+                return func()
+            except OperationalError:
+                continue
+
+        OperationalError()
--- test/interface_test/test_backend.py
+++ test/interface_test/test_backend.py
@@ -1,7 +1,19 @@
 import unittest
 import os
 import tempfile
+from multiprocessing.pool import Pool
 from interfaces import backend, schema
+
+
+def new_dataset(ind, database):
+    """Utility function to add single fake dataset to database"""
+
+    str_ind = str(ind)
+    accession = "LOC" + "0"*(6-len(str_ind)) + str_ind
+    test_entry = schema.Dataset(accession=accession,
+                                title="Test " + str_ind)
+    database.safe_add(test_entry)
+
 
 class TestDatabaseInit(unittest.TestCase):
     def test_init(self):
@@ -13,10 +25,47 @@ class TestDatabaseInit(unittest.TestCase):
             database = backend.DatabaseBackend(test_db_path)
             database.initialize_database()
 
-            engine = database._get_engine()
+            engine = database.create_engine()
             for name, cls in schema.__dict__.items():
                 if isinstance(cls, type) and hasattr(cls, "__tablename__"):
                     self.assertTrue(
                         engine.dialect.has_table(engine, cls.__tablename__)
                         )
 
+    def test_update(self):
+        """Test database updates in a single process"""
+
+        with tempfile.TemporaryDirectory() as temp_path:
+            test_db_path = "sqlite:///" + temp_path + "/phosphopedia.db"
+            database = backend.DatabaseBackend(test_db_path)
+            database.initialize_database()
+
+            new_dataset(1, database)
+
+            # Get entry back
+            session = database.create_session()
+            query = session.query(schema.Dataset)
+            retrieved_entry = database.safe_run(query.one)
+            self.assertEqual(int(retrieved_entry.accession[3:]), 1)
+
+            session.remove()
+
+    def test_update_parallel(self):
+        """Test database updates over multiple processes"""
+
+        with tempfile.TemporaryDirectory() as temp_path:
+            test_db_path = "sqlite:///" + temp_path + "/phosphopedia.db"
+            database = backend.DatabaseBackend(test_db_path)
+            database.initialize_database()
+
+            with Pool() as pool:
+                pool.starmap(new_dataset, [(ind, database) for ind in range(100)])
+
+            # Get all entries back
+            session = database.create_session()
+            query = session.query(schema.Dataset).order_by(schema.Dataset.accession)
+            retrieved_entries = database.safe_run(query.all)
+            for ind, entry in enumerate(retrieved_entries):
+                self.assertEqual(int(entry.accession[3:]), ind)
+
+            session.remove()
